85e19be08f94e2648c2798f9485cfae56b1f7352
Ruben Soerdien <ruben.soerdien@hotmail.com>
2019-03-30 14:05:36 +0100

    endless scroll

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { Pokemon} from '../models/pokemon';
 import { POKEMON } from '../mock-pokemon';
 import { PokemonService } from '../services/pokemon.service';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Species } from '../models/species';
+import { IonInfiniteScroll } from '@ionic/angular';
+
 @Component({
   selector: 'app-list',
   templateUrl: 'list.page.html',
@@ -12,15 +14,43 @@ import { Species } from '../models/species';
 })
 export class ListPage implements OnInit {
   private selectedItem: any;
-  pokemons = POKEMON;
-  listOfPokemon: Species[];
+  pokemons: Species[];
+  FullListOfPokemon: Species[];
+  fakeListOfPokemon: Array<any> = new Array(15);
+  // pokemonShown = 15;
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   constructor(private pokemonService: PokemonService) {
 
   }
 
   ngOnInit() {
-    this.pokemonService.getSpecies().subscribe(result => this.listOfPokemon = result);    
+    this.pokemonService.getSpecies().subscribe(result => this.setFullListOfPokemon(result));
+  }
+
+  setFullListOfPokemon(result: Species[]){
+    this.FullListOfPokemon = result;
+    this.pokemons = this.FullListOfPokemon.slice(0, 15);
+  }
+
+  loadData(event) {
+    var newData = this.FullListOfPokemon.slice(0, this.pokemons.length + 15);
+    // this.pokemons.concat(newData);
+    this.pokemons = newData;
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.pokemons.length >= this.FullListOfPokemon.length) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
 
 }
