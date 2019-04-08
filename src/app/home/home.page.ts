ef8161d5e90dab0bb864c62174ddf562b3264d7f
Ruben Soerdien <ruben.soerdien@hotmail.com>
2019-04-08 13:43:28 +0200

    fixx

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,31 +9,31 @@ import { Pokemon } from '../models/pokemon';
 })
 export class HomePage {
   items: Pokemon[] = [];
- 
+
   newItem: Pokemon = <Pokemon>{};
   @ViewChild('mylist')mylist: IonList;
   constructor(private storageService: PokemonStorageService, private plt: Platform, private toastController: ToastController) {
     this.plt.ready().then(() => {
       this.loadItems();
     });
-  } 
-  
+  }
+
   // READ
   loadItems() {
     this.storageService.getItems().then(items => {
       this.items = items;
     });
   }
-   
+
     // DELETE
     deleteItem(item: Pokemon) {
       this.storageService.deleteItem(item.id).then(item => {
-        this.showToast('Item removed!');
+        this.showToast('Pokemon removed!');
         this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
         this.loadItems(); // Or splice it from the array directly
       });
     }
-   
+
     // Helper
     async showToast(msg) {
       const toast = await this.toastController.create({
@@ -42,5 +42,5 @@ export class HomePage {
       });
       toast.present();
     }
-  
+
 }
